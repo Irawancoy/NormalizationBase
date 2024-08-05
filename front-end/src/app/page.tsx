e7ecd2aaa68b115f58bbf3b481301239536a1d3c
Irawancoy <septyoirawan8@gmail.com.Irawancoy>
2024-08-05 16:46:28 +0700

    update

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -15,46 +15,29 @@ const Page = () => {
   return (
     <>
       <HeaderComponent />
-      <Box component="table" sx={{ width: '100%' }}>
-      
-        <Box component="tbody">
-          <Box component="tr">
-            <Box
-              component="td"
-              sx={{
-                py: 2,
-                px: 6,
-              }}
-            >
-              <Stack direction="column" spacing={1} alignItems="stretch">
-                <EmployeeInformation employee={DATA.employee} />
-                <Stack direction="row" spacing={1} justifyContent="stretch">
-                  <Stack direction="column" spacing={1} sx={{ flexBasis: '50%', pb: 4 }}>
-                    <Typography variant="body1" sx={{ pt: 1 }}>
-                      <strong>Employment</strong>
-                    </Typography>
-                    <EmploymentTableComponent employment={DATA.histories.employment} />
-                  </Stack>
-                  <Divider orientation="vertical" flexItem />
-                  <ObjectivePersonalComponent biodata={DATA.employee.biodata} />
-                </Stack>
-                <Divider flexItem />
-                <Stack direction="row" spacing={1} justifyContent="stretch" sx={{ pb: 2, pageBreakAfter: 'always' }}>
-                  <CoursesTrainingComponent certifications={DATA.histories.certification} />
-                  <Divider orientation="vertical" flexItem />
-                  <EducationComponent education={DATA.histories.education} />
-                </Stack>
-                <ProjectsComponent projects={DATA.histories.project} />
-                <Typography variant="caption" color="GrayText" sx={{ pt: 4, alignSelf: 'center' }}>
-                  PT Padepokan Tujuh Sembilan
-                </Typography>
-              </Stack>
-            </Box>
-          </Box>
-        </Box>
-
-      
-      </Box>
+      <Stack spacing={2} sx={{ px: 6, py: 2 }}>
+        <EmployeeInformation employee={DATA.employee} />
+        <Stack direction="row" spacing={1} justifyContent="stretch">
+          <Stack direction="column" spacing={1} sx={{ flexBasis: '50%' }}>
+            <Typography variant="body1" sx={{ pt: 1 }}>
+              <strong>Employment</strong>
+            </Typography>
+            <EmploymentTableComponent employment={DATA.histories.employment} />
+          </Stack>
+          <Divider orientation="vertical" flexItem />
+          <ObjectivePersonalComponent biodata={DATA.employee.biodata} />
+        </Stack>
+        <Divider />
+        <Stack direction="row" spacing={1} justifyContent="stretch" sx={{ pb: 2 }}>
+          <CoursesTrainingComponent certifications={DATA.histories.certification} />
+          <Divider orientation="vertical" flexItem />
+          <EducationComponent education={DATA.histories.education} />
+        </Stack>
+        <ProjectsComponent projects={DATA.histories.project} />
+        <Typography variant="caption" color="GrayText" sx={{ pt: 4, alignSelf: 'center' }}>
+          PT Padepokan Tujuh Sembilan
+        </Typography>
+      </Stack>
       <FooterComponent />
     </>
   );
