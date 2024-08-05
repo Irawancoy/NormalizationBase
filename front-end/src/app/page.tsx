c330be2bb7fbf232ee62dc8a2e04b026149012a2
Irawancoy <septyoirawan8@gmail.com.Irawancoy>
2024-08-05 16:51:46 +0700

    update

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -1,5 +1,4 @@
 /* eslint-disable @next/next/no-img-element */
-
 import { Box, Divider, Stack, Typography } from '@mui/material';
 import { DATA } from '../../public/data';
 import HeaderComponent from '../components/HeaderComponent';
@@ -11,30 +10,32 @@ import EducationComponent from '@/components/EducationComponent';
 import ProjectsComponent from '@/components/ProjectComponent';
 import FooterComponent from '@/components/FooterComponents';
 
+
+
 const Page = () => {
   return (
     <>
       <HeaderComponent />
-      <Stack spacing={2} sx={{ px: 6, py: 2 }}>
+      <Stack spacing={2} className="container">
         <EmployeeInformation employee={DATA.employee} />
         <Stack direction="row" spacing={1} justifyContent="stretch">
           <Stack direction="column" spacing={1} sx={{ flexBasis: '50%' }}>
-            <Typography variant="body1" sx={{ pt: 1 }}>
+            <Typography variant="body1" className="headerSpacing">
               <strong>Employment</strong>
             </Typography>
             <EmploymentTableComponent employment={DATA.histories.employment} />
           </Stack>
-          <Divider orientation="vertical" flexItem />
+          <Divider orientation="vertical" flexItem className="verticalDivider" />
           <ObjectivePersonalComponent biodata={DATA.employee.biodata} />
         </Stack>
         <Divider />
         <Stack direction="row" spacing={1} justifyContent="stretch" sx={{ pb: 2 }}>
           <CoursesTrainingComponent certifications={DATA.histories.certification} />
-          <Divider orientation="vertical" flexItem />
+          <Divider orientation="vertical" flexItem className="verticalDivider" />
           <EducationComponent education={DATA.histories.education} />
         </Stack>
         <ProjectsComponent projects={DATA.histories.project} />
-        <Typography variant="caption" color="GrayText" sx={{ pt: 4, alignSelf: 'center' }}>
+        <Typography variant="caption" className="captionText">
           PT Padepokan Tujuh Sembilan
         </Typography>
       </Stack>
